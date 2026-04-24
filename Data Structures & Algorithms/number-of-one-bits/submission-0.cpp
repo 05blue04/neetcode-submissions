215b3ea6b1842cb46e63bc553932916e0657b206
Alexander Pienkowski <79887413+05blue04@users.noreply.github.com>
2026-04-24 17:02:35 -0500

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        uint32_t mask = 1;
+        int count = 0;
+
+        for(int i = 0; i < 32; i++){
+            uint32_t check = n & mask;
+            if(check > 0)
+                count++;
+            mask = mask << 1;
+        }
+
+        return count;
+    }
+};
